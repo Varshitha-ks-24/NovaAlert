1a4cba4696f72c10833956743bd72236b3a870f8
Varshitha K S <varshithaks@Varshithas-MacBook-Air.local>
2026-04-01 07:09:28 +0530

    fixed port for render

--- backend/app.py
+++ backend/app.py
@@ -29,4 +29,6 @@ def recommend():
     return jsonify(filtered)
 
 if __name__ == "__main__":
-   app.run(debug=True, port=5001)
+  import os
+
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
