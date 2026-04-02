ccadaa18ac0b492873693fae01f02c8f962c7860
Varshitha K S <varshithaks@Varshithas-MacBook-Air.local>
2026-04-02 06:49:27 +0530

    connected frontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ function App() {
   const [events, setEvents] = useState([]);
 
   const getRecommendations = async () => {
-    const res = await fetch("http://127.0.0.1:5001/recommend", {
+    const res = await fetch("https://novaalert.onrender.com/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
